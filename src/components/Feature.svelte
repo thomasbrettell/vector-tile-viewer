468e228567b2854d22340b39bc1a03233b6a3df4
Brettell <thomas.a.brettell@gmail.com>
2025-11-24 18:23:11 +1100

    add feature properties

--- src/components/Feature.svelte
+++ src/components/Feature.svelte
@@ -18,46 +18,48 @@
   let geometries = feature.loadGeometry();
 </script>
 
-{#if type === "Polygon"}
-  {#each geometries as geom}
-    {@const d = geom.reduce((prev, curr, i) => {
-      if (i === 0) {
-        prev += `M ${curr.x},${curr.y}`;
-      } else {
-        prev += `L ${curr.x},${curr.y}`;
-      }
+<g data-feature-id={feature.id} data-feature-name={feature.properties.name}>
+  {#if type === "Polygon"}
+    {#each geometries as geom}
+      {@const d = geom.reduce((prev, curr, i) => {
+        if (i === 0) {
+          prev += `M ${curr.x},${curr.y}`;
+        } else {
+          prev += `L ${curr.x},${curr.y}`;
+        }
 
-      return prev;
-    }, "")}
+        return prev;
+      }, "")}
 
-    <path fill={color} {d} />
-  {/each}
-{:else if type === "LineString"}
-  {#each geometries as geom}
-    {@const d = geom.reduce((prev, curr, i) => {
-      if (i === 0) {
-        prev += `M ${curr.x},${curr.y}`;
-      } else {
-        prev += `L ${curr.x},${curr.y}`;
-      }
-
-      return prev;
-    }, "")}
-
-    <path stroke={color} stroke-width={10} fill="none" {d} />
-  {/each}
-{:else if type === "Point"}
-  {#each geometries as geom}
-    {#each geom as point}
-      <circle
-        cx={point.x}
-        cy={point.y}
-        r="20"
-        fill-opacity={0.35}
-        stroke={color}
-        fill={color}
-        stroke-width={4}
-      />
+      <path fill={color} {d} />
     {/each}
-  {/each}
-{/if}
+  {:else if type === "LineString"}
+    {#each geometries as geom}
+      {@const d = geom.reduce((prev, curr, i) => {
+        if (i === 0) {
+          prev += `M ${curr.x},${curr.y}`;
+        } else {
+          prev += `L ${curr.x},${curr.y}`;
+        }
+
+        return prev;
+      }, "")}
+
+      <path stroke={color} stroke-width={10} fill="none" {d} />
+    {/each}
+  {:else if type === "Point"}
+    {#each geometries as geom}
+      {#each geom as point}
+        <circle
+          cx={point.x}
+          cy={point.y}
+          r="20"
+          fill-opacity={0.35}
+          stroke={color}
+          fill={color}
+          stroke-width={4}
+        />
+      {/each}
+    {/each}
+  {/if}
+</g>
